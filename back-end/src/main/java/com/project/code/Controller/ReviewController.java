3d2c5ad39bc0fb6056e432a6446c2843c903a5b0
TheRealPowerShellNinja <DavidTempleMCSE@GMail.Com>
2026-04-15 23:26:50 -0400

    Update ReviewController.java

--- back-end/src/main/java/com/project/code/Controller/ReviewController.java
+++ back-end/src/main/java/com/project/code/Controller/ReviewController.java
@@ -1,55 +1,61 @@
 package com.project.code.Controller;
 
-import com.project.code.Model.Customer;
-import com.project.code.Model.Review;
-import com.project.code.Repo.CustomerRepository;
-import com.project.code.Repo.ReviewRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.project.code.Model.Customer;
+import com.project.code.Model.Review;
+import com.project.code.Repo.CustomerRepository;
+import com.project.code.Repo.ReviewRepository;
 
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
 
     @Autowired
-    private ReviewRepository reviewRepository;
+    ReviewRepository reviewRepository;
 
     @Autowired
-    private CustomerRepository customerRepository;
+    CustomerRepository customerRepository;
 
     @GetMapping("/{storeId}/{productId}")
-    public Map<String, Object> getReviews(@PathVariable Long storeId, @PathVariable Long productId) {
-        Map<String, Object> response = new HashMap<>();
-        List<Map<String, Object>> reviewResponse = new ArrayList<>();
+    public Map<String,Object> getReviews(@PathVariable long storeId, @PathVariable long productId) {
+        Map<String, Object> map = new HashMap<>();
+        List<Review> reviews = reviewRepository.findByStoreIdAndProductId(storeId,productId);
 
-        List<Review> allReviews = reviewRepository.findAll();
+        List<Map<String, Object>> reviewsWithCustomerNames = new ArrayList<>();
 
-        for (Review review : allReviews) {
-            if (review.getStoreId().equals(storeId) && review.getProductId().equals(productId)) {
-                Map<String, Object> reviewMap = new HashMap<>();
-                reviewMap.put("comment", review.getComment());
-                reviewMap.put("rating", review.getRating());
+        for (Review review : reviews) {
+            Map<String, Object> reviewMap = new HashMap<>();
+            reviewMap.put("review", review.getComment());
+            reviewMap.put("rating", review.getRating());
 
-                Customer customer = customerRepository.findById(review.getCustomerId());
-                if (customer != null) {
-                    reviewMap.put("customerName", customer.getName());
-                } else {
-                    reviewMap.put("customerName", "Unknown");
-                }
-
-                reviewResponse.add(reviewMap);
+            Customer customer = customerRepository.findByid(review.getCustomerId());
+            if (customer != null) {
+                reviewMap.put("customerName", customer.getName());
+            } else {
+                reviewMap.put("customerName", "Unknown");
             }
+
+            reviewsWithCustomerNames.add(reviewMap);
         }
 
-        response.put("reviews", reviewResponse);
-        return response;
+        map.put("reviews", reviewsWithCustomerNames);
+        return map;
+    }
+
+    @GetMapping
+    public Map<String,Object> getAllReviews() {
+        Map<String,Object> map=new HashMap<>();
+        map.put("reviews",reviewRepository.findAll());
+        return map;
     }
 }
