1a4d025dd44b86d12b814d40f2fbf6e6cb0eb09f
TheRealPowerShellNinja <DavidTempleMCSE@GMail.Com>
2026-04-16 00:12:43 -0400

    Update OrderDetails.java

--- back-end/src/main/java/com/project/code/Model/OrderDetails.java
+++ back-end/src/main/java/com/project/code/Model/OrderDetails.java
@@ -1,18 +1,7 @@
 package com.project.code.Model;
 
 import java.time.LocalDateTime;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 @Entity
 public class OrderDetails {
@@ -21,77 +10,14 @@ public class OrderDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "customer_id")
-    @JsonManagedReference
-    private Customer customer;
-
+    // 🔴 MUST BE VISIBLE
     @ManyToOne
     @JoinColumn(name = "store_id")
-    @JsonManagedReference
     private Store store;
 
-    private Double totalPrice;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
     private LocalDateTime date;
-
-    @OneToMany(mappedBy = "order",fetch = FetchType.EAGER)
-    @JsonManagedReference
-    private List<OrderItem> orderItems;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public Store getStore() {
-        return store;
-    }
-
-    public void setStore(Store store) {
-        this.store = store;
-    }
-
-    public Double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(Double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public OrderDetails() {}
-
-    public OrderDetails(Customer customer, Store store, Double totalPrice, LocalDateTime date) {
-        this.customer = customer;
-        this.store = store;
-        this.totalPrice = totalPrice;
-        this.date = date;
-    }
 }
