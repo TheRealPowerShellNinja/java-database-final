b3341dca5e05eb8d373072b1461aa52b04bd380e
TheRealPowerShellNinja <DavidTempleMCSE@GMail.Com>
2026-04-15 23:52:33 -0400

    Update StoreController.java

--- back-end/src/main/java/com/project/code/Controller/StoreController.java
+++ back-end/src/main/java/com/project/code/Controller/StoreController.java
@@ -4,12 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.project.code.Model.PlaceOrderRequestDTO;
 import com.project.code.Model.Store;
@@ -26,34 +21,33 @@ public class StoreController {
     @Autowired
     private OrderService orderService;
 
-    @PostMapping
-    public Map<String, String> addStore(@RequestBody Store store) {
-        Store savedStore = storeRepository.save(store);
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "Store added successfully with id "+ savedStore.getId());
-        return map;
-    }
-
+    // 🔴 KEY: GET /validate/{storeId}
     @GetMapping("validate/{storeId}")
     public boolean validateStore(@PathVariable Long storeId ) {
-        Store store=storeRepository.findByid(storeId);
-        if(store!=null) {
+        Store store = storeRepository.findByid(storeId);
+        if (store != null) {
             return true;
         }
         return false;
     }
 
+    @PostMapping
+    public Map<String, String> addStore(@RequestBody Store store) {
+        Store savedStore = storeRepository.save(store);
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Store added successfully with id " + savedStore.getId());
+        return map;
+    }
+
     @PostMapping("/placeOrder")
     public Map<String,String> placeOrder(@RequestBody PlaceOrderRequestDTO placeOrderRequest) {
 
-        Map<String,String> map=new HashMap<>();
-        try{
+        Map<String,String> map = new HashMap<>();
+        try {
             orderService.saveOrder(placeOrderRequest);
             map.put("message","Order placed successfully");
-        }
-        catch(Error e) {
-            map.put("Error",""+e);
-
+        } catch (Error e) {
+            map.put("Error","" + e);
         }
         return map;
     }
