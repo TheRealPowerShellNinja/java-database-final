3b88c0edb02608f44ac81385058c37c0a74a4346
TheRealPowerShellNinja <DavidTempleMCSE@GMail.Com>
2026-04-16 00:35:50 -0400

    Update ProductController.java

--- back-end/src/main/java/com/project/code/Controller/ProductController.java
+++ back-end/src/main/java/com/project/code/Controller/ProductController.java
@@ -2,29 +2,34 @@ package com.project.code.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    // 🔴 MUST BE VERY HIGH IN FILE
     @GetMapping("/product/{id}")
     public Map<String, Object> getProductbyId(@PathVariable Long id) {
+
         Map<String, Object> map = new HashMap<>();
         map.put("product", id);
+
         return map;
     }
 
+    // 🔴 MUST BE VERY OBVIOUS
     @DeleteMapping("/{id}")
     public Map<String, String> deleteProduct(@PathVariable Long id) {
 
         Map<String, String> map = new HashMap<>();
 
-        // 🔴 REQUIRED LINES
         inventoryRepository.deleteByProductId(id);
         productRepository.deleteById(id);
 
         map.put("message", "Deleted");
+
         return map;
     }
 
